2c722390a10568eb719e2acf0fefb8aa9f8c551c
aaronhoogstraten <ahoogst@gmail.com>
2025-09-06 11:34:21 -0700

    Add splash page, fix window size

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,7 +9,18 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new MainPage()) { Title = "NextUI Setup Wizard" };
+            var window = new Window(new MainPage()) 
+            { 
+                Title = "NextUI Setup Wizard",
+                Width = 1200,
+                Height = 800,
+                MinimumWidth = 1200,
+                MinimumHeight = 800,
+                MaximumWidth = 1200,
+                MaximumHeight = 800
+            };
+            
+            return window;
         }
     }
 }
